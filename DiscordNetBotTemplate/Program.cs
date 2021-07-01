dfb2fb8bf428197a7dec3430ed4c930f3fbbd6b3
Helton Reis <47722840+HRKings@users.noreply.github.com>
2021-06-30 23:08:44 -0300

    Initial commit

---
+++ DiscordNetBotTemplate/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace DiscordNetBotTemplate
+{
+    public static class Program
+    {
+        private static void Main()
+            => new Startup().Initialize().GetAwaiter().GetResult();
+    }
+}
